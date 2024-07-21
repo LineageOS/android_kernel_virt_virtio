cea37292bb7d9b92670ba65f049e4998bbf67768
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-22 00:38:12 +0800

    [DNM] print virtio_gpu_translate_format
    Change-Id: I3a010e6cb15fd4cf3c5cf71f20e741af86641fa2

--- drivers/gpu/drm/virtio/virtgpu_plane.c
+++ drivers/gpu/drm/virtio/virtgpu_plane.c
@@ -51,52 +51,68 @@ uint32_t virtio_gpu_translate_format(uint32_t drm_fourcc)
 	switch (drm_fourcc) {
 #ifdef __BIG_ENDIAN
 	case DRM_FORMAT_XRGB8888:
+		pr_info("%s: be DRM_FORMAT_XRGB8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_X8R8G8B8_UNORM;
 		break;
 	case DRM_FORMAT_ARGB8888:
+		pr_info("%s: be DRM_FORMAT_ARGB8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_A8R8G8B8_UNORM;
 		break;
 	case DRM_FORMAT_BGRX8888:
+		pr_info("%s: be DRM_FORMAT_BGRX8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_B8G8R8X8_UNORM;
 		break;
 	case DRM_FORMAT_BGRA8888:
+		pr_info("%s: be DRM_FORMAT_BGRA8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_B8G8R8A8_UNORM;
 		break;
 	case DRM_FORMAT_RGBX8888:
+		pr_info("%s: be DRM_FORMAT_RGBX8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_R8G8B8X8_UNORM;
 		break;
 	case DRM_FORMAT_RGBA8888:
+		pr_info("%s: be DRM_FORMAT_RGBA8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_R8G8B8A8_UNORM;
 		break;
 	case DRM_FORMAT_XBGR8888:
+		pr_info("%s: be DRM_FORMAT_XBGR8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_X8B8G8R8_UNORM;
 		break;
 	case DRM_FORMAT_ABGR8888:
+		pr_info("%s: be DRM_FORMAT_ABGR8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_A8B8G8R8_UNORM;
 		break;
 #else
 	case DRM_FORMAT_XRGB8888:
+		pr_info("%s: le DRM_FORMAT_XRGB8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_B8G8R8X8_UNORM;
 		break;
 	case DRM_FORMAT_ARGB8888:
+		pr_info("%s: le DRM_FORMAT_ARGB8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_B8G8R8A8_UNORM;
 		break;
 	case DRM_FORMAT_BGRX8888:
+		pr_info("%s: le DRM_FORMAT_BGRX8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_X8R8G8B8_UNORM;
 		break;
 	case DRM_FORMAT_BGRA8888:
+		pr_info("%s: le DRM_FORMAT_BGRA8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_A8R8G8B8_UNORM;
 		break;
 	case DRM_FORMAT_RGBX8888:
+		pr_info("%s: le DRM_FORMAT_RGBX8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_X8B8G8R8_UNORM;
 		break;
 	case DRM_FORMAT_RGBA8888:
+		pr_info("%s: le DRM_FORMAT_RGBA8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_A8B8G8R8_UNORM;
 		break;
 	case DRM_FORMAT_XBGR8888:
+		pr_info("%s: le DRM_FORMAT_XBGR8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_R8G8B8X8_UNORM;
 		break;
 	case DRM_FORMAT_ABGR8888:
+		pr_info("%s: le DRM_FORMAT_ABGR8888\n", __func__);
 		format = VIRTIO_GPU_FORMAT_R8G8B8A8_UNORM;
 		break;
 #endif
