2d791fa7c09dd82b5136a26f3ac7f767ff1c4029
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-10 01:04:22 +0800

    HACK: selinux: Force permissive when `androidboot.selinux=permissive`
    * Helps debugging Android's user builds
    
    Change-Id: I5cec94d6db1e2ec68e2d6898919cf347d02d3372

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -159,6 +159,11 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 
 	new_value = !!scan_value;
 
+	if (strstr(saved_command_line, "androidboot.selinux=permissive")) {
+		pr_warn("SELinux: Force permissive as requested\n");
+		new_value = 0;
+	}
+
 	old_value = enforcing_enabled();
 	if (new_value != old_value) {
 		length = avc_has_perm(current_sid(), SECINITSID_SECURITY,
